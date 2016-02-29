fef8259e0f2d49f3c76acf062f05d8264d0e59fe
Jooyung Han <jooyung.han@gmail.com>
2016-02-29 12:26:35 +0900

    TicTacToe 추가 -- gametree까지만

---
+++ whyfp/tictactoe.hs
@@ -0,0 +1,36 @@
+import Data.List 
+
+data Tree a = Node a [Tree a] deriving (Show,Eq)
+foldtree f g a (Node label subtrees)= f label (foldr g a (map (foldtree f g a) subtrees))
+maptree f = foldtree (Node . f) (:) []
+reptree f a =  Node a (map (reptree f) (f a)) -- will be defined in TicTacToe example
+
+data Cell = E | O | X deriving (Show, Eq, Ord)
+type Position = [Cell]
+
+xflip (a:b:c:d:e:f:g:h:i:[]) = [c,b,a,f,e,d,i,h,g]
+yflip (a:b:c:d:e:f:g:h:i:[]) = [g,h,i,d,e,f,a,b,c]
+xyflip = yflip . rot90
+yxflip = xflip . rot90
+
+rot90 (a:b:c:d:e:f:g:h:i:[]) = [g,d,a,h,e,b,i,f,c]
+rot180 = rot90 . rot90
+rot270 = rot180 . rot90
+
+equiv p1 p2 = p1 `elem` sequence[id,xflip,yflip,xyflip,yxflip,rot90,rot180,rot270]p2
+
+moves p = nubBy equiv $ go p 
+            where go [] = []
+                  go (E:rest) = (player:rest) : map (E:) (go rest)
+                  go (x:rest) = map (x:) (go rest)
+                  player = turn p
+
+turn p = if no == nx then X else O
+            where f O (no,nx) = (no+1, nx)
+                  f X (no,nx) = (no, nx+1)
+                  f E (no,nx) = (no, nx)
+                  (no,nx) = foldr f (0,0) p
+
+gametree p = reptree moves p
+
+
